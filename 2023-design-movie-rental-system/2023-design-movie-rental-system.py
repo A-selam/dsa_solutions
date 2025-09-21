56ad67ab1ef6f621eb6c2c1db0c4f11cdbc5db81
A-selam <130240837+A-selam@users.noreply.github.com>
2025-09-21 21:56:02 +0300

    Time: 562 ms (68.33%), Space: 114.3 MB (49.17%) - LeetHub

---
+++ 2023-design-movie-rental-system/2023-design-movie-rental-system.py
@@ -0,0 +1,38 @@
+class MovieRentingSystem:
+
+    def __init__(self, n: int, entries: List[List[int]]):
+        self.price = {}
+        self.movie_to_shops = {}
+        self.rented = SortedList()
+
+        for shop, movie, price in entries:
+            self.price[(shop, movie)] = price
+            if movie not in self.movie_to_shops:
+                self.movie_to_shops[movie] = SortedList()
+            self.movie_to_shops[movie].add((price, shop))
+
+    def search(self, movie: int) -> List[int]:
+        if movie not in self.movie_to_shops:
+            return []
+        return [shop for price, shop in self.movie_to_shops[movie][:5]]
+
+    def rent(self, shop: int, movie: int) -> None:
+        price = self.price[(shop, movie)]
+        self.movie_to_shops[movie].remove((price, shop))
+        self.rented.add((price, shop, movie))
+
+    def drop(self, shop: int, movie: int) -> None:
+        price = self.price[(shop, movie)]
+        self.rented.remove((price, shop, movie))
+        self.movie_to_shops[movie].add((price, shop))
+
+    def report(self) -> List[List[int]]:
+        return [[shop, movie] for price, shop, movie in self.rented[:5]]
+        
+
+# Your MovieRentingSystem object will be instantiated and called as such:
+# obj = MovieRentingSystem(n, entries)
+# param_1 = obj.search(movie)
+# obj.rent(shop,movie)
+# obj.drop(shop,movie)
+# param_4 = obj.report()
