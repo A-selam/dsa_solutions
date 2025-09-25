f768701420de3788a85098a7e95740481cbbfbee
A-selam <130240837+A-selam@users.noreply.github.com>
2025-09-25 17:06:40 +0300

    Time: 3061 ms (5.03%), Space: 25.8 MB (40.11%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        memo = {}
+        def dp(i, k):
+            if i == len(stones)-1:
+                return True
+
+            state = (i, k)
+            if state in memo:
+                return memo[state]
+            temp = False
+            for j in range(i+1, len(stones)):
+                if stones[j] == stones[i] + k-1:
+                    temp = temp or dp(j, k-1)
+                if stones[j] == stones[i] + k:
+                    temp = temp or dp(j, k)
+                if stones[j] == stones[i] + k+1:
+                    temp = temp or dp(j, k+1)
+
+            memo[state] = temp
+            return temp
+        return dp(0, 0)            
+
