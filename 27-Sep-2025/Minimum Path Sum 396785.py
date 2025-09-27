b7957edc342b95cd31fa6a0b0098fde230d534be
A-selam <130240837+A-selam@users.noreply.github.com>
2025-09-27 15:23:31 +0300

    Created 'Minimum Path Sum' by A2SV Hub

---
+++ 27-Sep-2025/Minimum Path Sum 396785.py
@@ -0,0 +1,20 @@
+# Problem: Minimum Path Sum - https://leetcode.com/problems/minimum-path-sum/description/
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        memo = {}
+        def dp(i, j):
+            if i == n-1 and j == m-1:
+                return grid[i][j]
+            if i == n or j == m:
+                return float('inf')
+            
+            state = (i, j)
+            if state in memo:
+                return memo[state]
+            
+            memo[state] = grid[i][j] + min(dp(i+1, j), dp(i, j+1))
+            return memo[state]
+        return dp(0,0)
