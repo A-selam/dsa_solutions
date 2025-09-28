96b2091903e8cbfeb5aa30806cebc3a505456881
A-selam <130240837+A-selam@users.noreply.github.com>
2025-09-28 16:01:14 +0300

    Time: 161 ms (25.3%), Space: 66.4 MB (33.42%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        memo = {}
+        n = len(questions)
+        def dp(i):
+            if i >= n:
+                return 0
+            
+            if i in memo:
+                return memo[i]
+            
+            currPoints, currPower = questions[i]
+            memo[i] = max(dp(i+1), currPoints + dp(i+currPower+1))
+            return memo[i]
+
+        return dp(0)
