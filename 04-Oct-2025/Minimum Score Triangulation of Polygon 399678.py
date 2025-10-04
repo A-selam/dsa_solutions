05c9dabe5be94a4798f066e2c9e59f607fdff3b6
A-selam <130240837+A-selam@users.noreply.github.com>
2025-10-04 10:51:50 +0300

    Created 'Minimum Score Triangulation of Polygon' by A2SV Hub

---
+++ 04-Oct-2025/Minimum Score Triangulation of Polygon 399678.py
@@ -0,0 +1,16 @@
+# Problem: Minimum Score Triangulation of Polygon - https://leetcode.com/problems/minimum-score-triangulation-of-polygon/
+
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        n = len(values)
+        dp = [[0] * n for _ in range(n)]
+
+        for length in range(3, n + 1):  
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i][j] = float("inf")
+                for k in range(i + 1, j):
+                    dp[i][j] = min(dp[i][j],
+                                   dp[i][k] + dp[k][j] + values[i] * values[j] * values[k])
+               
+        return dp[0][n - 1]
