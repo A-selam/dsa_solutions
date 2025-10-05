e124d6039e3392cdaa54a52aa07b97c04cc24c2c
A-selam <130240837+A-selam@users.noreply.github.com>
2025-10-05 14:56:07 +0300

    Time: 4173 ms (5.01%), Space: 19.1 MB (91.81%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,8 +2,6 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         n = len(heights)
         m = len(heights[0])
-        atDir = [(1, 0), (0, 1)]
-        pacDir = [(-1, 0), (0, -1)]
         dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
         def inBound(row, col):
@@ -14,19 +12,15 @@ class Solution:
             visited = set([start])
             pacFlag = False
             atFlag = False
-            path = []
 
             while stack:
                 row, col = stack.pop()
-                # Do something with node
                 
                 if row == 0 or col == 0:
                     pacFlag = True
                 if row == n-1 or col == m-1:
                     atFlag = True
-                # print(row, col)
-                # path.append((row, col))
-                # check if pacific is reachable
+
                 for r, c in dir:
                     newR = row + r
                     newC = col + c
@@ -34,12 +28,12 @@ class Solution:
                         oldVal = heights[row][col]
                         newVal = heights[newR][newC]
                         if newVal <= oldVal:
-                            # print(True)
+                            
                             stack.append((newR, newC))
                             visited.add((newR, newC))
-                # print(stack)
+                
             return pacFlag and atFlag
-            # return path
+    
         ans = []
         for i in range(n):
             for j in range(m):
