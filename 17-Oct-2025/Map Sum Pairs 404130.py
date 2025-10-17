9c987324711b722ae765b0074cb61cbd76ed5851
A-selam <130240837+A-selam@users.noreply.github.com>
2025-10-17 22:08:40 +0300

    Created 'Map Sum Pairs' by A2SV Hub

---
+++ 17-Oct-2025/Map Sum Pairs 404130.py
@@ -0,0 +1,39 @@
+# Problem: Map Sum Pairs - https://leetcode.com/problems/map-sum-pairs/description/
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.score = 0
+
+class MapSum:
+
+    def __init__(self):
+        self.root = TrieNode()
+        self.map = {}
+
+    def insert(self, key: str, val: int) -> None:
+        to_add = val
+        if key in self.map:
+            to_add = val-self.map[key]
+        self.map[key] = val
+
+        curr = self.root
+        for char in key:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+            curr.score += to_add
+
+    def sum(self, prefix: str) -> int:
+        curr = self.root
+        for char in prefix:
+            if char not in curr.children:
+                return 0
+            curr = curr.children[char]
+        return curr.score
+
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
