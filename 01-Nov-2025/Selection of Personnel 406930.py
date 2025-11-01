03abbffdc140d522d2d0facc28ee5ef5e3891612
A-selam <130240837+A-selam@users.noreply.github.com>
2025-11-02 01:30:46 +0300

    Created 'Selection of Personnel' by A2SV Hub

---
+++ 01-Nov-2025/Selection of Personnel 406930.py
@@ -0,0 +1,8 @@
+# Problem: Selection of Personnel - https://codeforces.com/problemset/problem/630/F
+
+import math
+def solve():
+    n = int(input())
+    print(math.comb(n, 5) + math.comb(n, 6) + math.comb(n, 7))
+
+solve()
