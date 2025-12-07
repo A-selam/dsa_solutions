a57a2cbc0a83f18fb8e32c70531c4d2b2460961c
A-selam <130240837+A-selam@users.noreply.github.com>
2025-12-07 16:19:32 +0300

    Time: 0 ms (100%), Space: 3.7 MB (96.61%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.go
@@ -0,0 +1,10 @@
+func countOdds(low int, high int) int {
+    length := high - low + 1
+    if length%2 == 0 {
+        return length / 2
+    }
+    if low%2 == 1 {
+        return length/2 + 1
+    }
+    return length / 2
+}
