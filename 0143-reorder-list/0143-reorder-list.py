040dd0632f5a9c9d691a2f34efb289115e99c819
A-Selam <130240837+A-selam@users.noreply.github.com>
2026-08-21 17:18:45 +0300

    Time: 0 ms (100%), Space: 27.9 MB (21.67%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,19 +8,36 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        ptr = head 
-        stack = []
-        while ptr:
-            stack.append(ptr)
-            ptr = ptr.next
+        # break into half
+        slow = head
+        quick = head.next
+        while quick:
+            if not quick.next:
+                break 
+            slow = slow.next
+            quick = quick.next.next
+
+        # reverse the later half 
+        mid = slow.next
+        slow.next = None
+
+        ptr1 = None
+        ptr2 = mid
+        while ptr2:
+            ptr3 = ptr2.next
+            ptr2.next = ptr1
+            ptr1 = ptr2
+            ptr2 = ptr3
         
-        n = len(stack)
-        ptr = head
-        lst = None
-        for i in range(0, n//2):
-            cur = stack.pop()
-            temp = ptr.next
-            ptr.next = cur
-            cur.next = temp
-            ptr = temp
-        ptr.next = None
+        # merge
+        m_ptr1 = head
+        m_ptr2 = ptr1
+        while m_ptr1 and m_ptr2:
+            t_ptr1 = m_ptr1.next
+            t_ptr2 = m_ptr2.next
+            
+            m_ptr1.next = m_ptr2
+            m_ptr2.next = t_ptr1
+
+            m_ptr1 = t_ptr1
+            m_ptr2 = t_ptr2
